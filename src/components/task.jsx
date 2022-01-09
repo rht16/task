148bccc6670b56811ced685a9e31e95805e1cfb6
rht16 <rohitkrrt@gmail.com>
2022-01-09 15:45:33 +0530

    commenting

--- src/components/task.jsx
+++ src/components/task.jsx
@@ -1,68 +1,88 @@
-import react from 'react';
-import { useState } from 'react';
-import axios from 'axios';
-import  './task.css'
-function Task(){
-const[data, setData] = useState("")
-const [count, setCount] = useState(0)
-const [task, setTask] = useState("")
-const [value, setValue] = useState(false)
+import react from "react";
+import { useState } from "react";
+import axios from "axios";
+import "./task.css";
+function Task() {
+    //Managing the all states from here
+  const [data, setData] = useState("");
+  const [count, setCount] = useState(0);
+  const [task, setTask] = useState("");
+  const [value, setValue] = useState(false);
+    //It will run whenever Add button will click
+  const handle = () => {
+      // API counting
+    setCount(count + 1);
+    //Posting the data 
+    axios
+      .post("https://task-rht.herokuapp.com/add", {
+        task: data,
+      })
+      .then((res) => {
+        window.alert("Added");
 
-const handle=()=>{
-    setCount(count+1)
-  
-    axios.post('https://task-rht.herokuapp.com/add',{
-        task: data
-    }).then((res)=>{
-        window.alert("Added")
-         
-        setTask(data)
-     
-       
-    }).catch((err)=>{
+        setTask(data);
+      })
+      .catch((err) => {
         console.log(err);
-    })
-}
-
-const update=() => {
-    setValue(true)
-    
-}
-
-const updateReturn=() => {
-    setValue(false)
-    setCount(count+1)
-    axios.post('https://task-rht.herokuapp.com/update',{
-        task: data
-    }).then((res)=>{
-        window.alert("Updated")
-        setData("")
-        setTask(data)
-    }).catch((err)=>{
+      });
+  };
+  // It will invoke whenever Edit button click
+  const update = () => {
+    setValue(true);
+  };
+  // It will invoke whenever Update button click
+  const updateReturn = () => {
+    setValue(false);
+    //API call counting
+    setCount(count + 1);
+    //Updating the data
+    axios
+      .post("https://task-rht.herokuapp.com/update", {
+        task: data,
+      })
+      .then((res) => {
+        window.alert("Updated");
+        setData("");
+        setTask(data);
+      })
+      .catch((err) => {
         console.log(err);
-    })
-    
-}
-    return (
-        <>
-        <div className="parent">
+      });
+  };
+  return (
+    <>
+      <div className="parent">
         <h1>Task</h1>
         <br />
         {task}
         <br />
-        <input type="text" placeholder="enter task" value={data} onChange={(e)=>setData(e.target.value)} className="box_inp"/> 
+        <input
+          type="text"
+          placeholder="enter task"
+          value={data}
+          onChange={(e) => setData(e.target.value)}
+          className="box_inp"
+        />
         <br />
-        <button onClick={handle} className="btn_add">Add</button>
-        {value? <button onClick={updateReturn} className="btn_edit">Update</button>:<button onClick={update} className="btn_edit">Edit</button>}
+        <button onClick={handle} className="btn_add">
+          Add
+        </button>
+        {value ? (
+          <button onClick={updateReturn} className="btn_edit">
+            Update
+          </button>
+        ) : (
+          <button onClick={update} className="btn_edit">
+            Edit
+          </button>
+        )}
 
-    <br />
+        <br />
         <h5>Api called:{count}</h5>
         <br />
-        
-      
-        </div>
-        </>
-    )
+      </div>
+    </>
+  );
 }
 
 export default Task;
